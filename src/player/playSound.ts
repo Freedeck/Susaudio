e34135bbce278e84dbeb3305c92e963f8fde067a
Roi <68617740+Roizor@users.noreply.github.com>
2023-03-16 23:28:48 -0500

    webpack bein funy

--- src/player/playSound.ts
+++ src/player/playSound.ts
@@ -1,17 +1,18 @@
 import Susaudio from "../index"
+import * as _sa_ from "../globalFunctions"
 
-export default function (audioSource, audioName) {
+export default async function (audioSource: string, audioName: string) {
     if (Susaudio._player.default.timeSinceLastRequest < 2) return
     const audio = new Audio(audioSource)
-    audio.preservesPitch = Susaudio._player.preservesPitch
-    audio.playbackRate = Susaudio._player.pitch
+    audio.preservesPitch = Susaudio._player.default.preservesPitch
+    audio.playbackRate = Susaudio._player.default.pitch
     audio.isSusaudio = true
     audio.saName = audioName
-    await audio.setSinkId(Susaudio._player.sinkId)
+    await audio.setSinkId(Susaudio._player.default.sinkId)
     audio.play()
     audio.onended = () => {
-        Susaudio._player.queue = _sa_removeFromArray(Susaudio._player.queue, audio)
+        Susaudio._player.default.queue = _sa_.default.removeFromArray(Susaudio._player.default.queue, audio)
     }
-    Susaudio._player.queue.push(audio)
-    Susaudio._player.timeSinceLastRequest = 0
+    Susaudio._player.default.queue.push(audio)
+    Susaudio._player.default.timeSinceLastRequest = 0
 }
